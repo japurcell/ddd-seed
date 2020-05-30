601e0f1df16abed4e5167e9bb954e9e4490ebe07
japurcell <japurcell@users.noreply.github.com>
2020-05-30 20:51:25 +0000

    More vscode setup

--- ddd-seed/src/Psns.DDD/Entity.cs
+++ ddd-seed/src/Psns.DDD/Entity.cs
@@ -4,7 +4,7 @@ namespace Psns.DDD
     {
         int? _requestedHashCode;
         int _Id;
-        private List<INotification> _domainEvents;
+
         public virtual int Id
         {
             get
@@ -15,18 +15,6 @@ namespace Psns.DDD
             {
                 _Id = value;
             }
-        }
-
-        public List<INotification> DomainEvents => _domainEvents;
-        public void AddDomainEvent(INotification eventItem)
-        {
-            _domainEvents = _domainEvents ?? new List<INotification>();
-            _domainEvents.Add(eventItem);
-        }
-        public void RemoveDomainEvent(INotification eventItem)
-        {
-            if (_domainEvents is null) return;
-            _domainEvents.Remove(eventItem);
         }
 
         public bool IsTransient()
